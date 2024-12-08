f6b539c275a5f232fda6dfa8a1a90f633b4d0092
Sarthak Upadhyay <sarthak7u@gmail.com>
2024-12-08 11:05:29 +0530

    tweak terminal bg opacity and blur

--- wezterm/helpers.lua
+++ wezterm/helpers.lua
@@ -23,8 +23,8 @@ function module.apply_to_config(config)
   config.font = wezterm.font 'FiraCode Nerd Font'
   config.font_size = 18
   config.line_height = 1.2
-  config.window_background_opacity = 0.85
-  config.macos_window_background_blur = 50
+  config.window_background_opacity = 0.90
+  config.macos_window_background_blur = 30
   config.color_scheme = 'Gruvbox'
   config.enable_tab_bar = false
   config.window_decorations = 'RESIZE'
